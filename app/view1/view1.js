52a73690004af1ce6901edd15edca7a1572c6fc3
German Alvarez <ger.soto@gmail.com>
2016-07-05 01:18:48 +0200

    First implementation on ngModelController

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,36 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
 
+}])
+.directive('longNumber', [function () {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+
+      link: function (scope, element, attrs, ngModelController) {
+        //$formatters model->view --->pipeline runs array in reverse
+        //$parsers view->model --->pipeline runs array in order
+        var lastKeyPress=undefined;
+        ngModelController.$validators.text = function (modelValue, viewValue) {
+          return /^\d*([\.,]\d+)?$/.test(modelValue);
+        }
+
+        ngModelController.$parsers.unshift( function (text) {
+        	var transformedInput = text.replace(",",".");
+        	var transformedInput = text.replace(/[^\d\.,]/g,'')
+        	if (transformedInput !== text) {
+        		ngModelController.$setViewValue(transformedInput);
+        		ngModelController.$render();
+        	}
+        	return transformedInput;
+        });
+
+        element.bind("keydown keypress", function (event) {
+          lastKeyPress = event;
+          console.log("keydown keypress :" + JSON.stringify(event));
+        });
+        }
+    };
 }]);
